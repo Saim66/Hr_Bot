70a35718f421af4f2b67b085be2d87af4ce8130a
sayem <saimhere2004@gmail.com>
2026-07-03 19:48:37 +0600

    Cleanup: Removing unused files to stabilize project

--- bot.py
+++
@@ -1,8 +0,0 @@
-# bot.py
-from main import AdvancedBot
-import config
-
-# Point the legacy runner directly to your AdvancedBot class
-HighriseBot = AdvancedBot
-API_KEY = config.API_KEY
-ROOM_ID = config.ROOM_ID
